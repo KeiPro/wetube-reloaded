30a1f9283d97d3cf1e7760e303ff10b88d6bcf19
Kim youngwoo <keiprogramo@gmail.com>
2024-01-30 17:48:01 +0900

    bcrypt.compare를 이용하여 db에 저장되어 있는 해싱값을 비교하여 로그인 완료 처리가 되도록 기능 추가.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
 export const postJoin = async (req, res) => {
@@ -26,13 +27,18 @@ export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
 
 export const postLogin = async(req, res) => {
     const {username, password} = req.body;
-    const exists = await User.exists({username});
-    if(!exists){
-        return res.status(400).render("login", {pageTitle:"Login", errorMessage:"An Account with this username dest not exists."})
+    const pageTitle = "Login";
+    const user = await User.findOne({username});
+    if(!user){
+        return res.status(400).render("login", {pageTitle, errorMessage:"An Account with this username dest not exists."})
     }
     // Check if account exists
     // check if password correct
-    res.end();
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok){
+        return res.status(400).render("login", {pageTitle, errorMessage:"Wrong password"})
+    }
+    return res.redirect("/");
 }
 
 
