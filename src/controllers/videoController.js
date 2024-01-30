b2d1c2b9b3d78feb63a76abb7f4aa34fedfc7f13
Kim youngwoo <keiprogramo@gmail.com>
2024-01-30 17:07:32 +0900

    error시에 status code 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,7 @@ export const watch = async(req, res) => {
     const {id} = req.params; // ES6
     const video = await Video.findById(id);
     if(video === null){
-        return res.render("404", {pageTitle:"Video not found."});
+        return res.status(404).render("404", {pageTitle:"Video not found."});
     }
     return res.render("watch", {pageTitle:video.title, video});
 }
@@ -20,7 +20,7 @@ export const getEdit = async(req, res) => {
     const {id} = req.params; // ES6
     const video = await Video.findById(id);
     if(video === null){
-        return res.render("404", {pageTitle:"Video not found."});
+        return res.status(404).render("404", {pageTitle:"Video not found."});
     }
     return res.render("edit", {pageTitle:`Edit: ${video.title}`, video});
 }
@@ -56,7 +56,7 @@ export const postUpload = async (req, res) => {
         return res.redirect("/");
     }
     catch(error){
-        return res.render("upload", {pageTitle:`Upload Video`, errorMessage: error._message,});
+        return res.status(400).render("upload", {pageTitle:`Upload Video`, errorMessage: error._message,});
     }
 }
 
