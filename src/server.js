654907fe0c6f19d20038c945ffc72da8f1210526
Kim youngwoo <keiprogramo@gmail.com>
2024-01-30 15:22:21 +0900

    user model생성 및 createAccount관련 라우터 설정 및 유저 컨트롤러 추가중...

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -15,7 +15,7 @@ app.use(logger);
 // 우리가 쓸 수 있는 자바스크립트 형식으로 변형시켜 줄 수 있다.
 app.use(express.urlencoded({extended:true})); 
 
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
