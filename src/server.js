dfb00496ee2078ef04d8408b2fd2e2fcb41500a4
Kim youngwoo <keiprogramo@gmail.com>
2024-01-31 17:07:25 +0900

    .env 기능 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -18,9 +20,14 @@ app.use(logger);
 app.use(express.urlencoded({extended:true})); 
 
 app.use(session({
-    secret: "Hello!",
-    resave:true,
-    saveUninitialized: true,
+    secret: process.env.COOKEI_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 20000,
+    },
+    store: MongoStore.create({
+        mongoUrl:process.env.DB_URL}),
 }));
 
 app.use(localsMiddleware);
