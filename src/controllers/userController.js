51ede7d07710eb85b836c763a901eebaf06798ea
Kim youngwoo <keiprogramo@gmail.com>
2024-02-01 16:46:55 +0900

    중복 구문 리팩토링 및 postLogin에서 유저를 찾을 때 socialOnly가 false인(직접 사이트에서 계정을 만든 경우)경우에만 찾도록 코드 추가.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,7 +29,7 @@ export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
 export const postLogin = async(req, res) => {
     const {username, password} = req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({username});
+    const user = await User.findOne({username, socialOnly:false});
     if(!user){
         return res.status(400).render("login", {pageTitle, errorMessage:"An Account with this username dest not exists."})
     }
@@ -111,14 +111,10 @@ export const finishGithubLogin = async (req, res) => {
             return res.redirect("/login");    
         }
 
-        const existingUser = await User.findOne({email: emailObj.email});
-        if(existingUser){
-            req.session.loggedIn = true;
-            req.session.user = existingUser;
-            return res.redirect("/");
-        } else {
+        let user = await User.findOne({email: emailObj.email});
+        if(!user){
             // create an account
-            const user = await User.create({
+            user = await User.create({
                 name:userData.name, 
                 username: userData.login, 
                 email:emailObj.email,
@@ -126,11 +122,10 @@ export const finishGithubLogin = async (req, res) => {
                 socialOnly: true,
                 location:userData.location
             });
-
-            req.session.loggedIn = true;
-            req.session.user = user;
-            return res.redirect("/");
-        }
+        } 
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
         
     }else {
         return res.redirect("/login");
