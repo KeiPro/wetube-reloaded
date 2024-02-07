f24a24c283b0549136f8a5a345f5341aea9c8315
Kim youngwoo <keiprogramo@gmail.com>
2024-02-07 15:32:42 +0900

    timeline & 풀스크린 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,7 +3,10 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
-const totalTime = document.getElementById("totalTime")
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
  
 let volume = 0.5;
 video.volume = volume;
@@ -38,15 +41,37 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring
 
 const handleLoadedmetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 }
 
 const handleTimeUpdate = () => {
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
 };
+
+const handleTimelineChange = (event) => {
+    const {target: {value}} = event;
+
+    video.currentTime = value;
+}
+
+const handleFullscreen = () => {
+    const fullscreen = document.fullscreenElement;
+    if(fullscreen){
+        document.exitFullscreen();
+        fullScreenBtn.innerText = "Enter Full Screen";
+    }
+    else{
+        videoContainer.requestFullscreen();
+        fullScreenBtn.innerText = "Exit Full Screen";
+    }
+
+}
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleVolumeChange);
 video.addEventListener('timeupdate', handleTimeUpdate);
-
 video.readyState ? handleLoadedmetadata() : video.addEventListener("loadedmetadata", handleLoadedmetadata);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullscreen);
