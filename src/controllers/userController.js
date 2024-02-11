9dd4b40069bcd86cb85aa39c10f039e17aa19359
Kim youngwoo <keiprogramo@gmail.com>
2024-02-11 23:42:57 +0900

    flash는 session이 존재해야 하므로, session.destory하는 것 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -294,7 +294,9 @@ export const finishNaverLogin = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    req.session.destroy();
+    req.session.user = null;
+    res.locals.loggedInUser = req.session.user;
+    req.session.loggedIn = false;
     req.flash("info", "Bye Bye");
     return res.redirect("/");
 };
